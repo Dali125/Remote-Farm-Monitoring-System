0603446bc47521aba3fe43a8743285de44bf8133
dali22 <dalitsongulube@gmail.com>
2022-10-05 22:28:35 +0200

    more

--- app.js
+++ app.js
@@ -17,6 +17,26 @@ hiddenElements.forEach((el) => observer.observe(el));
 
 
 
+
+const observerforadmin = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show2')
+
+
+        } else {
+            entry.target.classList.remove('show2')
+
+        }
+    })
+})
+const hiddenElementsforadmin = document.querySelectorAll('.hidden2');
+hiddenElementsforadmin.forEach((el) => observer.observe(el));
+
+
+
 //Navigation barQuerry
 query = document.querySelector(".hamburger");
 query.onclick = function() {
